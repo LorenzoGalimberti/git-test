1e691df7b1521701db6aff585b9b01471454f66d
LorenzoGalimberti <lorenzogalimberti7996@gmail.com>
2023-10-03 16:13:56 +0200

    aggiunta home

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class DemoController {
 
+    public  String home(){
+        return "home";
+    }
 
 
 }
